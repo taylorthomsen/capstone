1e39d9511645a87bd08ba4a7f1006f5b4e889e86
Taylor Thomsen <taylor.thomsen@us.qbe.com>
2018-02-27 20:24:57 -0600

    change twitter key

--- twitter.py
+++ twitter.py
@@ -1,21 +1,22 @@
-## import the libraries
-import tweepy, codecs
+# import the libraries
+import tweepy
+import codecs
 
-## fill in your Twitter credentials 
-consumer_key = 'fzpCPYDGy4w5dNbOflgLmHDtb'
-consumer_secret = '4lyP3E8Z2GnKn7SiijztFMlWpLnEkGmEXiad7pEYUCFDcwPUfv'
-access_token = '727279051170181121DLwIHFIoJlMfDPVoH62NJ6NXcDBrlL1'
-access_token_secret = 'MiPhv7luIqWLYrUktTvODPfvqo6bbJZkxEXUtUtJCkIVH'
+# fill in your Twitter credentials
+consumer_key = 'kwrWHXJppr4uaDJb1yBBUe3Sy'
+consumer_secret = '1o1BvBTvBxvcyPqBdG6q1d8irBfl955IVnjPwn7ja0uYNaTvLA'
+access_token = '727279051170181121DVAUULh0SeCESGuDbuJn9rq4pvdXTGQ'
+access_token_secret = 'jeW3OS3Lg5YrQGKqJcuwX6vDHkFTbEL1mzbkdhbhIjqf2'
 
-## let Tweepy set up an instance of the REST API
+# let Tweepy set up an instance of the REST API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-## fill in your search query and store your results in a variable
+# fill in your search query and store your results in a variable
 results = api.search(q = "your search term here", lang = "en", result_type = "recent", count = 1000)
 
-## use the codecs library to write the text of the Tweets to a .txt file
+# use the codecs library to write the text of the Tweets to a .txt file
 file = codecs.open("twitterresults.txt", "w", "utf-8")
 for result in results:
 	file.write(result.text)
